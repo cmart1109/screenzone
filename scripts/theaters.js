a9637f73603f7f0351ff8e7f5d029b76b6675601
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-06-12 14:12:33 -0600

    change times

--- scripts/theaters.js
+++ scripts/theaters.js
@@ -12,7 +12,7 @@ let theaters = {
         },
         {
           "title": "Mission Impossible 8",
-          "showtimes": ["10:30 AM", "1:30 PM", "4:30 PM", "7:30 PM"]
+          "showtimes": ["4:30 PM", "7:30 PM"]
         },
         {
           "title": "Ballerina",
@@ -23,16 +23,8 @@ let theaters = {
           "showtimes": ["11:30 AM", "2:30 PM", "5:30 PM", "8:30 PM"]
         },
         {
-          "title": "Thunderbolts",
-          "showtimes": ["12:00 PM", "3:00 PM", "6:00 PM", "9:00 PM"]
-        },
-        {
           "title": "F1: The Movie",
           "showtimes": ["12:30 PM", "3:30 PM", "6:30 PM", "9:30 PM"]
-        },
-        {
-          "title": "Hurry Up Tomorrow",
-          "showtimes": ["1:00 PM", "4:00 PM", "7:00 PM", "10:00 PM"]
         },
         {
           "title": "Something Beautiful",
@@ -46,7 +38,7 @@ let theaters = {
       "movies": [
         {
           "title": "Lilo and Stitch",
-          "showtimes": ["9:45 AM", "12:45 PM", "3:45 PM", "6:45 PM"]
+          "showtimes": ["9:45 AM", "12:45 PM", "3:45 PM", "6:45 PM", "9:45 PM", "12:45 AM"]
         },
         {
           "title": "Mission Impossible 8",
@@ -62,7 +54,7 @@ let theaters = {
         },
         {
           "title": "Thunderbolts",
-          "showtimes": ["11:45 AM", "2:45 PM", "5:45 PM", "8:45 PM"]
+          "showtimes": ["11:45 AM", "2:45 PM", "5:45 PM", "8:45 PM", "11:45 PM"]
         },
         {
           "title": "F1: The Movie",
@@ -84,11 +76,7 @@ let theaters = {
       "movies": [
         {
           "title": "Lilo and Stitch",
-          "showtimes": ["10:00 AM", "1:00 PM", "4:00 PM", "7:00 PM"]
-        },
-        {
-          "title": "Mission Impossible 8",
-          "showtimes": ["10:20 AM", "1:20 PM", "4:20 PM", "7:20 PM"]
+          "showtimes": ["10:00 AM","11:00 AM", "1:00 PM", "4:00 PM", "7:00 PM"]
         },
         {
           "title": "Ballerina",
@@ -108,7 +96,7 @@ let theaters = {
         },
         {
           "title": "Hurry Up Tomorrow",
-          "showtimes": ["12:00 PM", "3:00 PM", "6:00 PM", "9:00 PM"]
+          "showtimes": ["9:00 PM"]
         },
         {
           "title": "Something Beautiful",
@@ -122,36 +110,28 @@ let theaters = {
       "movies": [
         {
           "title": "Lilo and Stitch",
-          "showtimes": ["9:30 AM", "12:30 PM", "3:30 PM", "6:30 PM"]
+          "showtimes": ["12:30 PM", "3:30 PM", "6:30 PM"]
         },
         {
           "title": "Mission Impossible 8",
-          "showtimes": ["10:00 AM", "1:00 PM", "4:00 PM", "7:00 PM"]
+          "showtimes": ["10:00 AM", "4:00 PM", "7:00 PM"]
         },
         {
           "title": "Ballerina",
-          "showtimes": ["10:30 AM", "1:30 PM", "4:30 PM", "7:30 PM"]
+          "showtimes": ["10:30 AM", "1:30 PM"]
         },
         {
           "title": "How to Train Your Dragon",
-          "showtimes": ["11:00 AM", "2:00 PM", "5:00 PM", "8:00 PM"]
+          "showtimes": ["11:00 AM", "12:00 PM", "2:00 PM", "5:00 PM", "8:00 PM"]
         },
         {
           "title": "Thunderbolts",
           "showtimes": ["11:30 AM", "2:30 PM", "5:30 PM", "8:30 PM"]
         },
         {
-          "title": "F1: The Movie",
-          "showtimes": ["12:00 PM", "3:00 PM", "6:00 PM", "9:00 PM"]
-        },
-        {
           "title": "Hurry Up Tomorrow",
-          "showtimes": ["12:30 PM", "3:30 PM", "6:30 PM", "9:30 PM"]
+          "showtimes": ["3:30 PM", "6:30 PM", "9:30 PM"]
         },
-        {
-          "title": "Something Beautiful",
-          "showtimes": ["1:00 PM", "4:00 PM", "7:00 PM", "10:00 PM"]
-        }
       ]
     }
   ]
