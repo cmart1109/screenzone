96ee6add72f4a7be07affda55731b9b97a78e549
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-06-12 14:08:06 -0600

    movies

--- scripts/theaters.js
+++ scripts/theaters.js
@@ -157,17 +157,69 @@ let theaters = {
   ]
 }
 
+let moviesImages = {
+  "movies": [
+    {
+      "title": "Lilo and Stitch",
+      "image": "./images/posters/stitch.png",
+      "synopsis": "LILO AND STITCH is the heartwarming and hilarious story of a Hawaiian girl and the runaway alien who helps her rebuild her family"
+    },
+    {
+      "title": "Mission Impossible 8",
+      "image": "./images/posters/mi8.png",
+      "synopsis": "Ethan and his team are on a mission to find and destroy an AI known as The Entity. The global journey results in incredible action scenes and more than one unexpected twist."
+    },
+    {
+      "title": "Ballerina",
+      "image": "./images/posters/ballerina.png",
+      "synopsis": "In this spin-off of John Wick, we meet Eve Macaro (Ana de Armas), who seeks revenge against all those who murdered her family."
+    },
+    {
+      "title": "How to Train Your Dragon",
+      "image": "./images/posters/httyd.webp",
+      "synopsis": "On the rugged Berk Island, where Vikings and dragons have held a bitter feud for generations, Hiccup is a boy unlike any other. The resourceful and underestimated son of Chief Stoick the Vast, he defies centuries of tradition by befriending Toothless, a feared Night Fury dragon."
+    },
+    {
+      "title": "Thunderbolts",
+      "image": "./images/posters/thunderbolts.webp",
+      "synopsis": "The Thunderbolts are a team of anti-heroes and reformed villains who undertake covert missions for the government. Led by John Walker (Sebastian Stan), the team includes Yelena Belova (Florence Pugh), Bucky Barnes (Sebastian Stan), and others with questionable pasts."
+    },
+    {
+      "title": "F1: The Movie",
+      "image": "./images/posters/f1.png",
+      "synopsis": "Nicknamed The Greatest Who Never Was, Sonny Hayes (Brad Pitt) was the most promising phenomenon of 1990s Formula One until his spectacular crash. Thirty years later, he's a nomadic driver for hire when he's approached by his former teammate Ruben Cervantes (Javier Bardem), owner of a struggling F1 team on the brink of collapse."
+    },
+    {
+      "title": "Hurry Up Tomorrow",
+      "image": "./images/posters/hut.png",
+      "synopsis": "Hurry Up Tomorrow is a 2025 American psychological thriller film directed by Trey Edward Shults from a screenplay by Shults, Abel the Weeknd Tesfaye, and Reza Fahim. It serves as a companion piece to Tesfaye's 2025 album of the same name and stars Tesfaye as a fictionalized version of himself, alongside Jenna Ortega and Barry Keoghan. In the film, Tesfaye, struggling with depression and insomnia, gets pulled into an existential odyssey by a mysterious fan."
+    },
+    {
+      "title": "Something Beautiful",
+      "image": "./images/posters/miley.png",
+      "synopsis": "Something Beautiful stands as a one-of-a-kind pop opera, where fantasy comes to life through the 13 original songs that make up Miley Cyrus's eponymous visual album. This cinematic experience was exclusively mixed by Alan Meyerson (Dune: Part One, Batman: The Dark Knight). The work was directed and written by Miley Cyrus, Jacob Bixenman, and Brendan Walter."
+    }
+  ]
+}
+
+
 
 function setShowtimes(moviesArray) {
     mainShowtimesContainer.innerHTML = '';
-    moviesArray.forEach(movie => {
+    moviesArray.forEach((movie, index) => {
         const movieContainer = document.createElement('div');
-        movieContainer.classList.add('movie');
+        movieContainer.classList.add('movie-showtimes');
         const movieTitle = document.createElement('h3');
         movieTitle.textContent = movie.title;
+        const movieImage = document.createElement('img');
+        movieImage.src = moviesImages.movies.find(m => m.title === movie.title).image; 
+        const movieSynopsis = document.createElement('p');
+        movieSynopsis.classList.add('movie-synopsis');
+        movieSynopsis.textContent = moviesImages.movies.find(m => m.title === movie.title).synopsis;
         const showtimesList = movie.showtimes  
         const showtimesContainer = document.createElement('div');
         showtimesContainer.classList.add('showtimes-container');
+        const showtimeBox = document.createElement('div');
         showtimesList.forEach(showtime => {
             const showtimeElement = document.createElement('span');
             showtimeElement.textContent = showtime;
@@ -175,7 +227,10 @@ function setShowtimes(moviesArray) {
             showtimesContainer.appendChild(showtimeElement);
         });
         movieContainer.appendChild(movieTitle);
+        movieContainer.appendChild(movieImage);
+        movieContainer.appendChild(movieSynopsis);
         movieContainer.appendChild(showtimesContainer);
+
         mainShowtimesContainer.appendChild(movieContainer);
     });
 }
